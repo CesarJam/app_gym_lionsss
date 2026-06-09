dd630ef99859fc7705dda52e974d04f829119cb2
Pablo Cesar Jimon <pablocesar-itech2025@hotmail.com>
2026-06-08 18:14:06 -0600

    feat: despliegue de frontend con nginx, configuración de túnel docker y conexión a supabase producción

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'http://127.0.0.1:54321'
-const supabaseKey = 'sb_publishable_ACJWlzQHLZjBrEguHvfOxg_3BJgxAaH'
+const supabaseUrl = 'https://api.lionsssacademy.com.mx'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlzcyI6InN1cGFiYXNlIiwiaWF0IjoxNzEwMDAwMDAwLCJleHAiOjIwMjYwMDAwMDB9._YcAXfm0orI7PqTfO7YJJq97tzwtA04vpFMGRo8jw-s'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
